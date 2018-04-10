84c2a70ec29bd2c01d2d7b3545c6520257036157
Lihou Yu <eudemonyu@gmail.com>
2018-04-09 21:42:01 -0500

    travis-config

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://iy:blog@ds153845.mlab.com:53845/blog_dev',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
